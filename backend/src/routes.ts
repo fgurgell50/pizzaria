03e97f1ed32fd710f745b09076a37b78fd024749
fgurgell50 <137560055+fgurgell50@users.noreply.github.com>
2024-02-05 19:05:17 -0300

    Commit List 05.02.24

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController"
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 const router = Router()
 
@@ -16,7 +17,7 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 // Rotas de Categorias
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
-router.get('')
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 //router.get("/teste", (req: Request, res: Response) => {
  //   return res.json({ nome: "Sujeito Pizza"})
