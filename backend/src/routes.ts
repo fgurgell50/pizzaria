5c532c6403726e6e34ed58519f6e88d61752b2b8
fgurgell50 <137560055+fgurgell50@users.noreply.github.com>
2024-04-16 17:17:05 -0300

    commit 10.04

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
+import multer from "multer";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController"
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
+import uploadConfig from './config/multer'
+
 
 const router = Router()
+
+const upload = multer(uploadConfig.upload("./tmp"))
 
 // Create User
 router.post("/user", new CreateUserController().handle)
@@ -18,6 +25,10 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 // Rotas de Categorias
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+// Rotas de Produtos
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+router.get('/category/products', isAuthenticated, new ListByCategoryController().handle  )
 
 //router.get("/teste", (req: Request, res: Response) => {
  //   return res.json({ nome: "Sujeito Pizza"})
