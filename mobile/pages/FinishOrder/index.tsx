1645f746a80e73d993f014793cddb666e55bdc82
fgurgell50 <137560055+fgurgell50@users.noreply.github.com>
2024-09-23 08:15:26 -0300

    Commit Mobile 23.09

---
+++ mobile/pages/FinishOrder/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { SafeAreaView, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+export default function FinishOrder(){
+    return(
+        <SafeAreaView style = { styles.container }>
+            <Text>TELA FINISH</Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+}) 
--- mobile/routes/app.routes.tsx
+++ mobile/routes/app.routes.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Dashboard from "@/pages/Dashboard"; 
 import Order from "@/pages/Order";
+import FinishOrder  from "@/pages/FinishOrder";
 
 
 export type StackParamsList = {
@@ -11,6 +12,7 @@ export type StackParamsList = {
         number: number | string
         order_id: string
     }
+    FinishOrder: undefined
 }
 
 const Stack = createNativeStackNavigator<StackParamsList>()
@@ -38,6 +40,13 @@ function AppRoutes(){
                 component={Order} 
                 options={{headerShown: false}}
              />
+
+            <Stack.Screen 
+                name="FinishOrder" 
+                component={FinishOrder} 
+                options={{headerShown: false}}
+             />
+
             </Stack.Navigator>
     )
 }
